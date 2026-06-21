e85f824f420502eaada8dc9e29d73fbc125ecfd9
jayrthrush2 <48532231+jayrthrush2@users.noreply.github.com>
2026-06-21 09:20:17 -0400

    Add files via upload

---
+++ GameVoiceText.cs
@@ -0,0 +1,13 @@
+﻿namespace Pelican_XVASynth
+{
+    public class GameVoiceText
+    {
+        public string gameId;
+        public int pitch;
+        public float rate;
+        public string text;
+        public string voiceId;
+        public float vol;
+        public string lang = "en"; // FIX: Provide default language fallback for v3.0 models
+    }
+}
---
+++ ModConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace Pelican_XVASynth
+{
+    public class ModConfig
+    {
+        public bool EnableMod { get; set; } = true;
+        public string NPCGameVoices { get; set; } = "";
+        public int MaxSecondsWait { get; set; } = 60;
+        public int MillisecondsPrepare { get; set; } = 1500;
+        public int MaxLettersToPrepare { get; set; } = 200;
+    }
+}
